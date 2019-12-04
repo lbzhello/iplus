052b2a5a0d899af7a931f5aa1da368ed86145756
lbzhello <lbzhello@qq.com>
2019-12-04 14:07:49 +0800

    update

---
+++ src/main/java/xyz/liujin/iplus/IplusApplication.java
@@ -0,0 +1,13 @@
+package xyz.liujin.iplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/liujin/iplus/IplusApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.liujin.iplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
