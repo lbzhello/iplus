61f948325299005c75d6c87621f82ecb5933d080
liubaozhu <liubaozhu@hikvision.com.cn>
2021-07-14 15:45:06 +0800

    Socket 相关代码修改

--- iplus-java/src/main/java/xyz/liujin/iplus/java/socket/FooClientSocket.java
+++ iplus-java/src/main/java/xyz/liujin/iplus/java/socket/FooClientSocket.java
@@ -5,27 +5,55 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 
 public class FooClientSocket {
     private static final Logger logger = LoggerFactory.getLogger(FooClientSocket.class);
 
+    private volatile boolean stop = false;
+
     public void startClient() {
-        try (Socket socket = new Socket("127.0.0.1", FooServerSocket.SERVER_PORT)) {
+        try {
             /**
              * 输入流和输出流必须在对流操作前获取，否则会会报错 Socket is closed
+             * socket 建立后可以重用，后续可以继续调用 readLine 获取消息
+             * 调用 socket 的输入流读取消息时，方法会阻塞，直至从流中收到消息
              */
+            Socket socket = new Socket("127.0.0.1", FooServerSocket.SERVER_PORT);
             // 打开输出流
-            try (PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-                 // 打开输入流
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+            // 打开输入流
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                // 向服务器发送消息
-                writer.println("hello, I'm from client");
-                writer.flush();
 
-                // 接收服务器消息
-                String msg = reader.readLine();
-                logger.info(msg);
+            // 接收线程，由于 readLine 方法会阻塞调，这里单独使用线程接收
+            new Thread(() -> {
+                try {
+                    String resp;
+                    while (true) {
+                        resp = reader.readLine();
+                        logger.info(resp);
+                    }
+                } catch (IOException e) {
+                    logger.error("failed to receive msg from server", e);
+                }
+            }).start();
+
+            // 发送线程
+            try {
+                BufferedReader system = new BufferedReader(new InputStreamReader(System.in));
+                String msg;
+                do {
+                    msg = system.readLine();
+                    // 向服务器发送消息
+                    writer.println(msg);
+                    writer.flush();
+                } while (!Objects.equals(msg, FooServerSocket.END_STR));
+                // 关闭输入流
+                writer.close();
+                reader.close();
+            } catch (IOException e) {
+                logger.error("failed to send msg to server socket");
             }
 
         } catch (IOException e) {
--- iplus-java/src/main/java/xyz/liujin/iplus/java/socket/FooServerSocket.java
+++ iplus-java/src/main/java/xyz/liujin/iplus/java/socket/FooServerSocket.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,6 +14,9 @@ public class FooServerSocket {
     private static final Logger logger = LoggerFactory.getLogger(FooServerSocket.class);
 
     public static final int SERVER_PORT = 8888;
+
+    // 输入 q 停止客户端连接
+    public static final String END_STR = "q";
 
     private static ExecutorService executorService = Executors.newFixedThreadPool(1);
 
@@ -29,6 +33,8 @@ public class FooServerSocket {
                         try {
                             /**
                              * 输入流和输出流必须在对流操作前获取，否则会会报错 Socket is closed
+                             * socket 建立后可以重用，后续可以继续调用 readLine 获取消息
+                             * 调用 socket 的输入流读取消息时，方法会阻塞，直至从流中收到消息
                              */
                             // 打开输入流
                             try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -36,12 +42,16 @@ public class FooServerSocket {
                                  PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())))) {
 
                                 // 接收客户端消息
-                                String msg = reader.readLine();
-                                logger.info(msg);
+                                String msg;
+                                do {
+                                    msg = reader.readLine();
+                                    logger.info(msg);
 
-                                // 向客户端发送消息
-                                writer.println("hello, I'm from server.");
-                                writer.flush();
+                                    // 向客户端发送消息
+                                    writer.println("hello, I'm server. I received your message: " + msg);
+                                    writer.flush();
+                                } while (!Objects.equals(msg, END_STR));
+
                             }
 
                             socket.close();
