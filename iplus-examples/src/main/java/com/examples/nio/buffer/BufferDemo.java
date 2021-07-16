290b49e4ec2bf688113630ad52581b53ba1bcc25
liubaozhu <liubaozhu@hikvision.com.cn>
2021-07-16 17:08:54 +0800

    新增 BufferUtils

--- iplus-examples/src/main/java/com/examples/nio/buffer/BufferDemo.java
+++ iplus-examples/src/main/java/com/examples/nio/buffer/BufferDemo.java
@@ -1,6 +1,8 @@
 package com.examples.nio.buffer;
 
+import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
 
 /**
  *
@@ -8,9 +10,24 @@ import java.nio.CharBuffer;
 public class BufferDemo {
 
     public static void main(String[] args) {
-        CharBuffer charBuffer = CharBuffer.allocate(128);
-        charBuffer.append("你好");
+        ByteBuffer bb = ByteBuffer.allocate(256);
 
-        System.out.println();
+        // 写入数据
+        bb.put("hello world".getBytes(StandardCharsets.UTF_8));
+
+        // 转成读模式, limit = position; position = 0
+        bb.flip();
+
+        CharBuffer cb = StandardCharsets.UTF_8.decode(bb);
+        System.out.println(cb.toString());
+
+
+        // 转换写模式， limit
+        bb.clear();
+        bb.put("hihi".getBytes(StandardCharsets.UTF_8));
+
+        bb.flip();
+        System.out.println(StandardCharsets.UTF_8.decode(bb).toString());
+
     }
 }
